a29bc52dbd362fbf9636850bf76f39227ecc4282
jylmarcus <maplecow@gmail.com>
2023-06-09 15:38:32 +0800

    Testing issues

--- src/main/java/vttp2023/batch3/ssf/frontcontroller/FrontcontrollerApplication.java
+++ src/main/java/vttp2023/batch3/ssf/frontcontroller/FrontcontrollerApplication.java
@@ -1,12 +1,33 @@
 package vttp2023.batch3.ssf.frontcontroller;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FrontcontrollerApplication {
+	@Value("${spring.redis.host}")
+    private static String redisHost;
+    
+
+    @Value("${spring.redis.port}")
+    private static Optional<Integer> redisPort;
+    
+
+    @Value("${spring.redis.username}")
+    private static String redisUsername;
+    
+
+    @Value("${spring.redis.password}")
+    private static String redisPassword;
 
 	public static void main(String[] args) {
+		System.out.println(redisHost);
+		System.out.println(redisPort);
+		System.out.println(redisUsername);
+		System.out.println(redisPassword);
 		SpringApplication.run(FrontcontrollerApplication.class, args);
 	}
 
