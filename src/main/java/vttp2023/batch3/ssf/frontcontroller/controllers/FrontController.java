675e38cf26f25e2aea79946764ce33c5970878dc
jylmarcus <maplecow@gmail.com>
2023-06-09 11:01:07 +0800

    Completed task 2

--- src/main/java/vttp2023/batch3/ssf/frontcontroller/controllers/FrontController.java
+++ src/main/java/vttp2023/batch3/ssf/frontcontroller/controllers/FrontController.java
@@ -1,5 +1,6 @@
 package vttp2023.batch3.ssf.frontcontroller.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,27 +8,34 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 import jakarta.validation.Valid;
 import vttp2023.batch3.ssf.frontcontroller.model.LoginForm;
+import vttp2023.batch3.ssf.frontcontroller.services.AuthenticationService;
 
 @Controller
 public class FrontController {
+
+	@Autowired
+	AuthenticationService service;
 
 	@RequestMapping("/")
 	public String index(Model model){
 		model.addAttribute("loginForm", new LoginForm());
 		return "view0";
 	}
-	// TODO: Task 2, Task 3, Task 4, Task 6
 
 	@PostMapping(path="/login", consumes= MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-	public String login(@Valid @ModelAttribute("loginForm") LoginForm loginForm, BindingResult bindingResult, Model model) {
+	public String login(@Valid @ModelAttribute("loginForm") LoginForm loginForm, BindingResult bindingResult, Model model) throws Exception {
 		if(bindingResult.hasErrors()) {
 			return "view0";
 		}
+		if(!service.authenticate(loginForm.getUsername(), loginForm.getPassword())){
+			
+		}
+
 		return "view1";
 	}
+
+	// TODO: Task 2, Task 3, Task 4, Task 6
 	
 }
--- src/main/java/vttp2023/batch3/ssf/frontcontroller/services/AuthenticationService.java
+++ src/main/java/vttp2023/batch3/ssf/frontcontroller/services/AuthenticationService.java
@@ -1,11 +1,45 @@
 package vttp2023.batch3.ssf.frontcontroller.services;
 
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import jakarta.json.Json;
+import jakarta.json.JsonObject;
+
+@Service
 public class AuthenticationService {
 
+	private String AUTH_URL = "https://authservice-production-e8b2.up.railway.app/api/authenticate";
 	// TODO: Task 2
 	// DO NOT CHANGE THE METHOD'S SIGNATURE
 	// Write the authentication method in here
-	public void authenticate(String username, String password) throws Exception {
+	public boolean authenticate(String username, String password) throws Exception {
+		JsonObject json = Json.createObjectBuilder()
+			.add("username", username)
+			.add("password", password)
+			.build();
+
+		RequestEntity<String> req = RequestEntity
+			.post(AUTH_URL)
+			.contentType(MediaType.APPLICATION_JSON)
+			.header("Accept", MediaType.APPLICATION_JSON_VALUE)
+			.body(json.toString(), String.class);
+
+		RestTemplate template = new RestTemplate();
+
+		ResponseEntity<String> resp = template.exchange(req, String.class);
+		//System.out.println(resp.getStatusCode());
+		//System.out.println(resp.getBody());
+		
+		if(resp.getStatusCode().is4xxClientError()) {
+			return false;
+		}
+
+		return true;
 	}
 
 	// TODO: Task 3
